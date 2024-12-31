49726322b589a48ca54353f07a4b56708fcfe38f
Aashish Kumar Ray <2002raysi@gmail.com>
2024-12-31 21:54:49 +0530

    First Reach Component

--- first-react-component/src/App.jsx
+++ first-react-component/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CompButton from "./CompButton";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App(){
+  return  <div className="container">
+    <header className="heading">This is my first react program</header>
+    <CompButton/>
+  </div>
 }
 
-export default App
+export default App;
+
+
+// But theere is one more way of  exproting the component are the named exporting
+
+// export default function App(){
+//   return  <div className="container">
+//     <header className="heading">This is my first react program</header>
+//     <CompButton/>
+//   </div>
+// }
+
+// export default App;
---
+++ first-react-component/src/CompButton.jsx
@@ -0,0 +1,6 @@
+
+function CompButton(){
+    return <button>Click Me</button>
+}
+
+export default CompButton;
