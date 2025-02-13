97f27fac58c42ad922db7bba720668c60a40d68b
Aashish Kumar Ray <2002raysi@gmail.com>
2025-02-13 16:14:02 +0530

    General concepts in react

--- general-concepts/src/App.jsx
+++ general-concepts/src/App.jsx
@@ -1,10 +1,15 @@
 import Hello from './Hello'
 import './App.css'
+import Random from "./ReusableComponent"
 
 function App() {
   return(
     <>
     <Hello />
+    <Random />
+    <Random />
+    <Random />
+    <Random />
     </>
   )
 }
---
+++ general-concepts/src/ReusableComponent.jsx
@@ -0,0 +1,9 @@
+function Random(){
+    return(
+        <p>
+            Random Number: {Math.floor(Math.random() * 100)}
+        </p>
+    )
+}
+
+export default Random;
