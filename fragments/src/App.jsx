1d3e88cf98fc32e9c849144f427faff62e200e32
Aashish Kumar Ray <2002raysi@gmail.com>
2025-02-15 16:32:05 +0530

    Fragments

---
+++ fragments/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>My Diet</h1>
+      </div>
+      <ul class="list-group">
+        <li class="list-group-item" aria-current="true">4 Eggs</li>
+        <li class="list-group-item">100gm Soya Chunks</li>
+        <li class="list-group-item">100gm Black Grams</li>
+        <li class="list-group-item">150gm Tomato</li>
+        <li class="list-group-item">100gm Onion</li>
+        <li class="list-group-item">15gm Chilly</li>
+        <li class="list-group-item">10gm Lemone</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
