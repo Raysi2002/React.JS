d69399f6bd547e32d9a9acd15cbe74bf2afad4d1
Aashish Kumar Ray <2002raysi@gmail.com>
2025-02-18 15:07:15 +0530

    If Else Condition

--- conditional-statements/src/App.jsx
+++ conditional-statements/src/App.jsx
@@ -1,13 +1,10 @@
+import IfComponent from "./components/IfComponent"
+
 function App() {
   const itemList = ['Eggs', 'Carrot', 'Soya Chunks', 'Lemon'];
   return (
     <>
-      <h1>Conditional Statements</h1>
-      <ul className="list-group">
-            {
-                itemList.map(item => <li key={item} className="list-group-item">{item}</li>)
-            }
-        </ul>
+      <IfComponent></IfComponent>
     </>
   )
 }
--- conditional-statements/src/components/IfComponent.jsx
+++ conditional-statements/src/components/IfComponent.jsx
@@ -1,3 +1,19 @@
 function IfElse(){
-    
+    // const food = ['Kichdi', 'Mutton', 'Banana'];
+    const food = [];
+    if(food.length === 0){
+        return <h1>No Food Items</h1>
+    }
+    return (
+        <>
+        <h1>Food Items</h1>
+        <ul>
+            {
+                food.map(item => <li key={item} className="list-group-item"> {item} </li>)
+            }
+        </ul>
+        </>
+    )
 }
+
+export default IfElse;
