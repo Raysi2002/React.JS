6d2a8efd1f40850410b793cc419dac0740aee6de
Aashish Kumar Ray <2002raysi@gmail.com>
2025-02-27 13:19:21 +0530

    state management

--- use-state/src/components/List.jsx
+++ use-state/src/components/List.jsx
@@ -2,11 +2,12 @@ import css from "./List.module.css";
 import Input from "./Input"
 import DeleteBtn from "./DeleteBtn";
 
-const List = ({foodList}) => {
+
+const List = ({foodList, handleClick}) => {
     return (
         <ul className="list-group">
             {
-                foodList.map(item => <li key={item} id="listItem" className="list-group-item">{item} <DeleteBtn /> </li>)
+                foodList.map(item => <li onClick={() => handleClick} key={item} id="listItem" className="list-group-item">{item} <DeleteBtn /> </li>)
             }
         </ul>
     )
